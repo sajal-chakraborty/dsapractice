5727a9ad85142d64ea18a3882f1cbe11f32d3b73
“Sajal <sajal.chakraborty@gmail.com>
2025-12-19 19:35:34 +0530

    Two Sum

---
+++ src/main/java/com/test/dsa/dsapractice/algorithms/TwoSum.java
@@ -0,0 +1,27 @@
+package com.test.dsa.dsapractice.algorithms;
+
+import java.util.HashMap;
+
+public class TwoSum {
+
+
+    
+     public int[] twoSum(int[] nums, int target) {
+        
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int[] positions = new int[2];
+        for(int i = 0 ; i < nums.length;i++){
+            Integer complement = target - nums[i];
+            if(!map.containsKey(complement)){
+                map.put(nums[i],i);
+            }else{
+                positions[0] = map.get(complement);
+                positions[1] = i;
+                break;
+            }
+        
+        }
+        return positions;
+
+    }
+}
